0186a8c5c8e6c90dccf7c766d65b1e9476b91130
Catalin Radoi <radoi.catalin88@gmail.com>
2023-12-22 00:14:47 +0100

    asd

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 //const eminem = "https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem
 
-window.onload = function () {
-  fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem", {
-    method: "GET",
-    //headers
-  })
-    .then(function (response) {
+const fetchSongs = () => {
+  fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
+    .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
         throw response.satus;
       }
     })
-    .then(function (canzoni) {
-      const row = document.querySelector(".row");
-      for (const song of canzoni) {
-        row.innerHTML = `<div class="card" style="width: 18rem;">
-  <img src="" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${song.title}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>`;
+    .then((canzoni) => {
+      console.log("dica");
+      const riga = document.querySelector(".row");
+
+      for (let song of canzoni) {
+        riga.innerHTML = `<div class="card" id="card-${song}">
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${song.title}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">${song.type}</a>
+        </div>
+      </div>`;
       }
     })
-    .catch(function (err) {
+    .catch((err) => {
       console.error(err);
     });
 };
+
+fetchSongs();
