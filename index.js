5a4eb128ac4b4c1b46dcb9e2db670b1f89a61a2c
Catalin Radoi <radoi.catalin88@gmail.com>
2023-12-21 23:41:38 +0100

    asd

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ window.onload = function () {
     //headers
   })
     .then(function (response) {
-      console.log(response);
       if (response.ok) {
         return response.json();
       } else {
-        //errore
+        throw response.satus;
       }
     })
     .then(function (canzoni) {
-      console.log(canzoni);
+      const row = document.querySelector(".row");
+      for (const song of canzoni) {
+        row.innerHTML = `<div class="card" style="width: 18rem;">
+  <img src="" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${song.title}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`;
+      }
+    })
+    .catch(function (err) {
+      console.error(err);
     });
 };
